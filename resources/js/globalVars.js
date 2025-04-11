581acde49131982f4b259d18a34cba06a6fbe1ad
Johnathank78 <johnathan.kancel@gmail.com>
2025-04-11 23:05:44 +0200

    update

--- resources/js/globalVars.js
+++ resources/js/globalVars.js
@@ -760,7 +760,7 @@ $(document).ready(function(){
 
     $("img").attr("draggable", false);
     
-    
+    /*KONSOLEADD
 
     konsole = new Konsole($(".konsole")[0]);
     window.onerror = function(message, error) {
@@ -778,6 +778,6 @@ $(document).ready(function(){
         consoleShown = !consoleShown;
     })
 
-    
+    KONSOLEADD*/
     
 });//readyEnd
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v6.38';
+const CACHE_NAME = 'app-cache-v6.39';
 
 self.addEventListener('install', event => {
     event.waitUntil(
